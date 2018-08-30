2c9de0849c1201ef3562bcc34f74611527934a45
Uilian Ries <uilianries@gmail.com>
2018-08-29 23:22:18 -0300

    Build on Linux
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -1,83 +1,63 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from conans import ConanFile, CMake, tools
+from conans import ConanFile, AutoToolsBuildEnvironment, tools
 import os
 
 
-class LibnameConan(ConanFile):
-    name = "libname"
-    version = "0.0.0"
-    description = "Keep it short"
-    url = "https://github.com/bincrafters/conan-libname"
-    homepage = "https://github.com/original_author/original_lib"
+class Libmowgli2Conan(ConanFile):
+    name = "libmowgli-2"
+    version = "2.1.3"
+    description = "Generic runtime for atheme applications "
+    url = "https://github.com/bincrafters/conan-libmowgli-2"
+    homepage = "https://github.com/atheme/libmowgli-2"
     author = "Bincrafters <bincrafters@gmail.com>"
-    # Indicates License type of the packaged library
     license = "MIT"
-
-    # Packages the license for the conanfile.py
     exports = ["LICENSE.md"]
-
-    # Remove following lines if the target lib does not use cmake.
-    exports_sources = ["CMakeLists.txt"]
-    generators = "cmake"
-
-    # Options may need to change depending on the packaged library.
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = "shared=False", "fPIC=True"
-
-    # Custom attributes for Bincrafters recipe conventions
+    options = {"shared": [True, False], "fPIC": [True, False], "with_openssl": [True, False]}
+    default_options = "shared=False", "fPIC=True", "with_openssl=True"
     source_subfolder = "source_subfolder"
-    build_subfolder = "build_subfolder"
-
-    # Use version ranges for dependencies unless there's a reason not to
-    # Update 2/9/18 - Per conan team, ranges are slow to resolve.
-    # So, with libs like zlib, updates are very rare, so we now use static version
-
-
-    requires = (
-        "OpenSSL/[>=1.0.2l]@conan/stable",
-        "zlib/1.2.11@conan/stable"
-    )
+    autotools = None
 
     def config_options(self):
         if self.settings.os == 'Windows':
-            del self.options.fPIC
+            self.options.remove("fPIC")
+
+    def configure(self):
+        del self.settings.compiler.libcxx
+
+    def requirements(self):
+        if self.options.with_openssl:
+            self.requires.add("OpenSSL/1.1.0g@conan/stable")
 
     def source(self):
-        source_url = "https://github.com/libauthor/libname"
-        tools.get("{0}/archive/v{1}.tar.gz".format(source_url, self.version))
+        tools.get("{0}/archive/v{1}.tar.gz".format(self.homepage, self.version))
         extracted_dir = self.name + "-" + self.version
-
-        #Rename to "source_subfolder" is a convention to simplify later steps
         os.rename(extracted_dir, self.source_subfolder)
 
-    def configure_cmake(self):
-        cmake = CMake(self)
-        cmake.definitions["BUILD_TESTS"] = False # example
-        if self.settings.os != 'Windows':
-            cmake.definitions['CMAKE_POSITION_INDEPENDENT_CODE'] = self.options.fPIC
-        cmake.configure(build_folder=self.build_subfolder)
-        return cmake
+    def configure_autotools(self):
+        if not self.autotools:
+            args = ['--disable-examples']
+            args.extend(['--enable-shared', '--disable-static'] if self.options.shared else ['--enable-static', '--disable-shared'])
+            args.append('--with-openssl' if self.options.with_openssl else '--without-openssl')
+            self.autotools = AutoToolsBuildEnvironment(self, win_bash=tools.os_info.is_windows)
+            with tools.chdir(self.source_subfolder):
+                self.autotools.configure(args=args)
+        return self.autotools
 
     def build(self):
-        cmake = self.configure_cmake()
-        cmake.build()
+        autotools = self.configure_autotools()
+        with tools.chdir(self.source_subfolder):
+            autotools.make()
 
     def package(self):
-        self.copy(pattern="LICENSE", dst="licenses", src=self.source_subfolder)
-        cmake = self.configure_cmake()
-        cmake.install()
-        # If the CMakeLists.txt has a proper install method, the steps below may be redundant
-        # If so, you can just remove the lines below
-        include_folder = os.path.join(self.source_subfolder, "include")
-        self.copy(pattern="*", dst="include", src=include_folder)
-        self.copy(pattern="*.dll", dst="bin", keep_path=False)
-        self.copy(pattern="*.lib", dst="lib", keep_path=False)
-        self.copy(pattern="*.a", dst="lib", keep_path=False)
-        self.copy(pattern="*.so*", dst="lib", keep_path=False)
-        self.copy(pattern="*.dylib", dst="lib", keep_path=False)
+        self.copy(pattern="COPYING", dst="licenses", src=self.source_subfolder)
+        autotools = self.configure_autotools()
+        with tools.chdir(self.source_subfolder):
+            autotools.install()
 
     def package_info(self):
         self.cpp_info.libs = tools.collect_libs(self)
+        if self.settings.os == "Linux":
+            self.cpp_info.libs.append("pthread")
